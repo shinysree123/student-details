52c1dae212df31bffcbdce2115f562ef8ea17a81
shinysree123 <shinyjoseph89@gmail.com>
2020-04-03 12:33:23 +0530

    student details

---
+++ studentdetail.js
@@ -0,0 +1,24 @@
+var express =require('express');
+var bodyParser =require('body-parser');
+
+var app=express();
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/',(req, res )=>{
+
+    res.send("hai..");
+});
+
+app.get('/students',(req, res )=>{
+
+    var name =req.body.getname;
+    var roll =req.body.getroll;
+    var adminNo=req.body.getadminNo;
+    var college=req.body.getclg;
+
+    res.json(req.body);
+});
+
+app.listen(process.env.PORT || 3222, () => {
+    console.log("server started");
+});
